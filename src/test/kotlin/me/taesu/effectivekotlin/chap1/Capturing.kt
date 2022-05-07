5fffc73be9c39f4d52e9bc62528c8ee340a39177
Lee Tae Su <taesu@crscube.co.kr>
2022-05-07 14:59:09 +0900

    Chapter1 Capturing.kt
    yields는 리턴의 의미, 피보나치

--- src/test/kotlin/me/taesu/effectivekotlin/chap1/Capturing.kt
+++ src/test/kotlin/me/taesu/effectivekotlin/chap1/Capturing.kt
@@ -57,7 +57,7 @@ class Capturing {
         assertThat(primes).isNotEqualTo((listOf(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)))
     }
 
-     @Test
+    @Test
     fun `소수 구하기 기본 예제 시퀀스 변수 선언 안하는 버전`() {
         val sequence = sequence {
             var numbers = generateSequence(2) { it + 1 }
@@ -72,5 +72,37 @@ class Capturing {
         val primes = sequence.take(15).toList()
         println(primes)
         assertThat(primes).isEqualTo(listOf(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47))
+    }
+
+    @Test
+    fun `yield test`() {
+        val sequence = sequence {
+            yield(1)
+            yieldAll(listOf(3, 4, 5))
+            yieldAll(generateSequence(7) { it + 2 })
+        }
+
+        val numbers = sequence.take(10).toList()
+        println(numbers)
+        assertThat(numbers).isEqualTo(listOf(1, 3, 4, 5, 7, 9, 11, 13, 15, 17))
+    }
+
+    @Test
+    fun `yield 피보나치`(){
+        // 1 1 2 3 5
+        val sequence = sequence {
+            var pair = 0 to 1
+            yield(pair.first + pair.second)
+            while (true) {
+                (pair.first + pair.second).apply {
+                    yield(this)
+                    pair = pair.second to this
+                }
+            }
+        }
+
+        val numbers = sequence.take(10).toList()
+        println(numbers)
+        assertThat(numbers).isEqualTo(listOf(1, 1, 2, 3, 5, 8, 13, 21, 34, 55))
     }
 }
