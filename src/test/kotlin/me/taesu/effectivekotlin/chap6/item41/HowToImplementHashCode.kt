dabf5399c66fb458de5fdd18c46c9ae9d8067401
Lee Tae Su <taesu@crscube.co.kr>
2022-07-15 10:22:24 +0900

    Item41 hashCode의 규약을 지켜라
    JPA인 경우 사항 추가

--- src/test/kotlin/me/taesu/effectivekotlin/chap6/item41/HowToImplementHashCode.kt
+++ src/test/kotlin/me/taesu/effectivekotlin/chap6/item41/HowToImplementHashCode.kt
@@ -49,6 +49,14 @@ class HowToImplementHashCode {
         SameHashCode("686") in toSet
         println(SameHashCode.equalCount)    // 391
         println(SameHashCode.hashCount)
+
+        SameHashCode.equalCount = 0
+        SameHashCode.hashCount = 0
+
+        println("\nafter to set")
+        List(100) { SameHashCode("$it") }.toSet()
+        println(SameHashCode.equalCount)    // 5469
+        println(SameHashCode.hashCount)
     }
 }
 
@@ -77,6 +85,24 @@ class User(
         return pureKotlinHash(name, age)
     }
 
+    /*
+        https://www.youtube.com/watch?v=a_6V8xwiv04&t=2928s
+        https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+
+        만약 GeneratedValue를 사용하는  Entity라면
+        영속화 되기 전, 후의 hashCode가 일치하지 않을 수 있음
+
+        그래서 차라리 클래스 별 고정된 hashcode를 반환하게 하는 것을 고려하기도 함.
+        HashSet, HashMap에 이 Entity를 담는다면 끔찍할 수 있으나
+        한꺼번에 많은수의 Entity를 fetch 해올일이 없기에(성능상의 이유로) 괜찮다?
+        -> 그래도 100개 정도 불러왔을 때 5469, 100 번의 연산이 실행되기에 무시할수는 없을 것 같다.
+
+        However, for performance reasons, you should always limit the number of entities that are stored in a collection.
+        You should never fetch thousands of entities in a @OneToMany Set
+        because the performance penalty on the database side is multiple orders of magnitude
+        higher than using a single hashed bucket.
+
+     */
     fun thisClassHashCode(): Int {
         return this::class.hashCode()
     }
